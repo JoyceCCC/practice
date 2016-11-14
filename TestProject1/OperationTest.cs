7732d40848a3189367fe56015d890789cc5c11d5
JoyceCCC <myyedmm1992@163.com>
2016-11-14 21:03:48 +0800

    Initial commit

---
+++ TestProject1/OperationTest.cs
@@ -0,0 +1,104 @@
+﻿using maxLength;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///This is a test class for OperationTest and is intended
+    ///to contain all OperationTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class OperationTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for getLength
+        ///</summary>
+        [TestMethod()]
+        public void getLengthTest()
+        {
+            string strs1 = string.Empty; 
+            int expected1 = 0; 
+            int actual1;
+            actual1 = Operation.getLength(strs1);
+            Assert.AreEqual(expected1, actual1);
+
+            string strs2 = "abcabcbb";
+            int expected2 = 3;
+            int actual2;
+            actual2 = Operation.getLength(strs2);
+            Assert.AreEqual(expected2, actual2);
+
+            string strs3 = "aBcAbcBb";
+            int expected3 = 3;
+            int actual3;
+            actual3 = Operation.getLength(strs3);
+            Assert.AreEqual(expected3, actual3);
+
+            string strs4 = "a";
+            int expected4 = 1;
+            int actual4;
+            actual4 = Operation.getLength(strs4);
+            Assert.AreEqual(expected4, actual4);
+
+            string strs5 = "abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+            int expected5 = 0;
+            int actual5;
+            actual5 = Operation.getLength(strs5);
+            Assert.AreEqual(expected5, actual5);
+        }
+    }
+}
---
+++ maxLength/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace maxLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Operation
+    {
+        public static int getLength(string str)
+        {
+            int length = 0;
+	        int max = 1;
+	        int count = 0;
+	        int i=0,j=0;
+
+	        int[] a = new int[26];
+            while (i < 26)
+                a[i++] = 0;
+
+	        length = str.Length;
+            if (length == 0)
+                return 0;
+            else if (length == 1)
+                return 1;
+            else if (length > 100)
+                return 0;
+
+	        string str1 = str.ToLower();
+
+            char[] str2 = str1.ToCharArray();
+             
+	        a[str2[0] -'a'] = 1;
+
+	        i = 0;
+	        j = 1;
+	        count = 1;
+	        while((j < length) && (i < length - max))
+	        {
+		        if(a[str2[j] - 'a'] == 0)
+		        {
+			        a[str2[j] - 'a'] = 1;
+			        count++;
+			        if(max < count)
+				        max = count;
+			        j++;
+		        }
+		        else 
+		        {
+			        a[str2[i] - 'a'] = 0;
+			        i++;
+			        count--;
+		        }
+	        }
+	        return max;
+        }
+    }
+}
